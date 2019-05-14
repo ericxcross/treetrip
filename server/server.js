a38a25f12e3a5af5004fc12f1a1b3fa5e343cba8
ericxcross <eric.cross@sympatico.ca>
2019-05-14 22:26:47 +0100

    server updated with heroku port

--- server/server.js
+++ server/server.js
@@ -2,11 +2,19 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const parser = require("body-parser");
+const port = process.env.PORT || 3000;
+
+app.set('view engine', 'ejs');
+
 
 const publicPath = path.join(__dirname, "../client/public");
 app.use(express.static(publicPath));
 app.use(parser.json());
 
-app.listen(3000, function() {
+app.get('/', function(req, res) {
+  res.render('index');
+});
+
+app.listen(port, function() {
   console.log(`Listening on port ${this.address().port}`);
 });
